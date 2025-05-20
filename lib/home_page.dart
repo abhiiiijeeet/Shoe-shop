abce895ce484a19a12cf0f30e1ac721a2db7372e
am4t3rasu <abhijeetdubey7224@gmail.com>
2025-05-21 00:54:21 +0530

    initial commit

---
+++ lib/home_page.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> filters = const [
+    'All',
+    "Addidas",
+    'Nike',
+    'Bata'
+  ];
+
+  late String selectedFilter;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    selectedFilter = filters[0];
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    const border = OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color.fromRGBO(225, 225, 225, 1)
+                        ),
+                        borderRadius: BorderRadius.horizontal(left: Radius.circular(50))
+                        
+                      );
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text("Shoes\nCollections",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 35
+                  ), 
+                  ),
+                ),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      
+                      hintText: "Search",
+                      prefixIcon: Icon(Icons.search),
+                      border: border,
+                      enabledBorder: border,
+                      focusedBorder: border,                      
+                    ),
+
+                  ))
+              ],
+            ),
+            SizedBox(
+              height: 120,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: filters.length,
+                itemBuilder: (context, index){
+                  final filter = filters[index];
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      selectedFilter = filter;
+                      
+                    },
+                    child: Chip(
+                      backgroundColor:selectedFilter == filter? Theme.of(context).colorScheme.primary: Color.fromRGBO(245, 247, 249, 1),
+                      side: BorderSide(
+                        color: Color.fromRGBO(245, 247, 249, 1)
+                      ),
+                      label: Text(filter),
+                      labelStyle: TextStyle(fontSize: 16),
+                      padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30)
+                      ),),
+                  ),
+                );
+              },),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_app/home_page.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    
+      theme: ThemeData(
+        useMaterial3: false,
+        fontFamily: 'Lato',
+        inputDecorationTheme: InputDecorationTheme(
+          hintStyle: TextStyle(
+            fontSize: 16, 
+            fontWeight: FontWeight.w700
+          ),
+          prefixIconColor: Color.fromRGBO(119, 119, 119, 1)
+        ),
+        colorScheme: ColorScheme.fromSeed(seedColor:Color.fromRGBO(254, 206, 1,1),
+        primary: Color.fromRGBO(254, 206, 1,1))
+      ),
+      
+      debugShowCheckedModeBanner: false,
+      title: 'Shopping App',
+      home: HomePage(),
+    );
+  }
+}
